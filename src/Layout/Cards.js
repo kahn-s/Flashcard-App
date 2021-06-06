44116a521dae1464129595d8359f2c3834db3b93
Shari Kahn <kahn.shari2@gmail.com>
2021-06-06 13:00:17 +0300

    additional initial stuff

--- src/Layout/Cards.js
+++ src/Layout/Cards.js
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
 
-export default function Cards({ card }) {
+export default function Cards({ deck }) {
   const [flip, setFlip] = useState(false);
+  const card = deck.map((card) => (
+    <div className="card">
+      <div className="card-body">
+        <h6 className="card-subtitle">{`Card ${card.id} of {deck.length}`}</h6>
+        <div classname="front">
+          <p className="card-text">{card.front}</p>
+        </div>
+        <div classname="back">
+          <p className="card-text">{card.back}</p>
+        </div>
+      </div>
+    </div>
+  ));
 
   return (
     <section>
--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -7,49 +7,25 @@ import Cards from "./Cards";
 
 // TODos  Routes to Edit Card, Add Card
 
-export const Study = () => {
+function Study() {
   const [deck, setDeck] = useState([]);
   const [error, setError] = useState(undefined);
 
   const deckId = useParams();
-  const sampleCard = {
-    id: 1,
-    front: "Differentiate between Real DOM and Virtual DOM.",
-    back: "Virtual DOM updates are faster but do not directly update the HTML",
-    deckId: 1,
-  };
+
   console.log(deckId);
+
   useEffect(() => {
     const abortController = new AbortController();
 
-    listDecks(abortController.signal)
-      .then((value) => {
-        setDeck(value.filter((deck) => deck.id === deckId));
-      })
-      .catch(setError);
+    readDeck(deckId, abortController.signal).then(setDeck).catch(setError);
 
     return () => abortController.abort();
-  }, [deckId]);
+  }, []);
 
   if (error) {
     return <ErrorMessage error={error} />;
   }
-
-  deck.map((card) => {
-    return (
-      <>
-        <Cards card={card} key={card.id} className="card" />;
-        <div className="card-body" key={card.id}>
-          <h5 className="card-title">{`Card ${card.id} of ${deck.length}`}</h5>
-          <p className="card-text">{card}</p>
-
-          <button type="button" className="btn btn-primary">
-            Flip
-          </button>
-        </div>
-      </>
-    );
-  });
 
   return (
     <section>
@@ -71,9 +47,10 @@ export const Study = () => {
       </div>
 
       <div>
-        <Cards />
+        <Cards deck={deck} />
       </div>
     </section>
   );
-};
+}
+
 export default Study;
